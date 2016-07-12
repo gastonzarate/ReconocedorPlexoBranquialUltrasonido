bb91a6e521a8f896bb4703e67a497a8f016e00d8
gastonzarate <gaston.z195@gmail.com>
2016-07-12 13:18:50 -0300

    Agrego comparar imagenes

--- ReducirRuido.py
+++ ReducirRuido.py
@@ -4,10 +4,7 @@ from skimage import img_as_float
 from skimage.restoration import denoise_nl_means,inpaint,denoise_tv_chambolle, denoise_bilateral,denoise_tv_bregman
 
 
-def filtroMorfologico(path,iterations=1):
-    # Cargar la imagen
-    imagen = cv2.imread(path, 0)
-
+def filtroMorfologico(imagen,iterations=1):
     # Crear un kernel de '1' de 3x3
     kernel = np.ones((3, 3), np.uint8)
 
@@ -16,25 +13,23 @@ def filtroMorfologico(path,iterations=1):
 
     return transformacion
 
-def denoiseNonLocalMeans(path):
+def denoiseNonLocalMeans(imagen):
     """
     Reemplaza la intensidad de cada pixel con la media de los pixels a su alrededor
     """
-    imagen = cv2.imread(path, 0)
     noisy = img_as_float(imagen)
 
     denoise = denoise_nl_means(noisy, 7, 9, 0.08)
 
     return denoise
 
-def denoiseBilateral(path,multichannel):
+def denoiseBilateral(imagen,multichannel):
     """
     -Reemplaza el valor de cada pixel en funcion de la proximidad espacial y radiometrica
      medida por la funcion Gaussiana de la distancia euclidiana entre dos pixels y con
      cierta desviacion estandar.
     -False si la imagen es una escala de grises, sino True
     """
-    imagen = cv2.imread(path, 0)
     noisy = img_as_float(imagen)
 
     denoise = denoise_bilateral(noisy, 7, 9, 0.08,multichannel)
@@ -42,23 +37,21 @@ def denoiseBilateral(path,multichannel):
     return denoise
 
 
-def denoiseTV_Chambolle(path,multichannel):
+def denoiseTV_Chambolle(imagen,multichannel):
     """
     -Tiende a producir imagenes como las de los dibujos animados.
     -Reduce al minimo la variacion total de la imagen
     """
-    imagen = cv2.imread(path, 0)
     noisy = img_as_float(imagen)
 
     denoise = denoise_tv_chambolle(noisy, 7, 9, 0.08,multichannel)
 
     return denoise
 
-def denoiseTV_Bregman(path,isotropic):
+def denoiseTV_Bregman(imagen,isotropic):
     """
     -isotropic es el atributo para cambiar entre filtrado isotropico y anisotropico
     """
-    imagen = cv2.imread(path, 0)
     noisy = img_as_float(imagen)
 
     denoise = denoise_tv_bregman(noisy, 7, 9, 0.08, isotropic)
--- iniciador.py
+++ iniciador.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import re
+import ReducirRuido
 
 #Funcion para ordenar los nombres de las imagenes
 def compara(a, b):
@@ -19,17 +20,47 @@ def compara(a, b):
 
     return retorno
 
+def buscarImagenesIguales(imgs):
+    lista = []
+    for i in range(len(imgs)):
+        for j in range(len(imgs)):
+            comp = compararImagenes(imgs[i],imgs[j])
+            if(comp==True):
+              lista.append("%i - %i",(i,j))
+            if(i==j):
+                break
+    return lista
+
+def compararImagenes(a,b):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3), (1, 1))
+    img = cv2.subtract(a, b)
+    img,thresh = cv2.threshold(img, 15, 255, cv2.CV_THRESH_BINARY)
+    img = cv2.morphologyEx(img, cv2.CV_MOP_OPEN, kernel, (-1, -1), 1)
+    im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    if(len(contours)==0):
+        bandera = True
+    else:
+        bandera = False
+    return bandera
+
 #Carga las imagenes en memoria
 imgs = []
 mask = []
-for base,dirs,files in os.walk('train'):
+for base,dirs,files in os.walk('train2'):
     files.sort(cmp=compara)
     for name in files:
         print os.path.join(base, name)
         i = cv2.imread(os.path.join(base, name),cv2.IMREAD_GRAYSCALE)
-        if 'mask' in files:
+
+        #i = ReducirRuido.filtroMorfologico(i,iterations=5)
+        #i = ReducirRuido.denoiseNonLocalMeans(i)
+        #cv2.imshow('image', i)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+        if 'mask' in name:
             mask.append(i)
         else:
             imgs.append(i)
-
+    lista = buscarImagenesIguales(imgs)
+    print lista
 
