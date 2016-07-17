1945a01b0fe325209e181df60b6cfad87166d4d7
gastonzarate <gaston.z195@gmail.com>
2016-07-17 13:49:44 -0300

    Subo el separador de imagenes
    Crea carpetas pos y neg donde guarda las correspondientes imagenes y los
    archivos pos.info y neg.info donde guarda las referencias de cada
    imagen, en el archivo pos.info tambien guarda la posicion del plexo en
    cada imagen

--- buscadorImagenesIguales.py
+++ buscadorImagenesIguales.py
@@ -1,3 +1,4 @@
+
 import os
 import cv2
 import re
@@ -19,24 +20,26 @@ def compara(a, b):
         retorno = 1
 
     return retorno
-
 def buscarImagenesIguales(imgs):
     lista = []
     for i in range(len(imgs)):
         for j in range(len(imgs)):
+            print "%d - %d" %(i,j)
+            if (i == j):
+                break
             comp = compararImagenes(imgs[i],imgs[j])
             if(comp==True):
-              lista.append("%i - %i",(i,j))
-            if(i==j):
-                break
+              lista.append("%d - %d" %(i,j))
+
     return lista
 
 def compararImagenes(a,b):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3), (1, 1))
     img = cv2.subtract(a, b)
-    img,thresh = cv2.threshold(img, 15, 255, cv2.CV_THRESH_BINARY)
-    img = cv2.morphologyEx(img, cv2.CV_MOP_OPEN, kernel, (-1, -1), 1)
+    img,thresh = cv2.threshold(img, 15, 255, cv2.THRESH_BINARY)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
     if(len(contours)==0):
         bandera = True
     else:
@@ -52,11 +55,6 @@ for base,dirs,files in os.walk('train2'):
         print os.path.join(base, name)
         i = cv2.imread(os.path.join(base, name),cv2.IMREAD_GRAYSCALE)
 
-        #i = ReducirRuido.filtroMorfologico(i,iterations=5)
-        #i = ReducirRuido.denoiseNonLocalMeans(i)
-        #cv2.imshow('image', i)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
         if 'mask' in name:
             mask.append(i)
         else:
