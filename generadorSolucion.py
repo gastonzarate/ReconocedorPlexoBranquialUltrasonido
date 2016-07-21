58bd48f8655fd6e1d55f327b3f0eab5d496ce982
gastonzarate <gaston.z195@gmail.com>
2016-07-21 04:51:07 -0300

    Corrijo Generador Solucion

--- generadorSolucion.py
+++ generadorSolucion.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import ReducirRuido
+import numpy as np
+from skimage import img_as_ubyte
 
 ruta = "submission.csv"
 # Pregunta si existen los directorios o archivos para eliminarlos
@@ -24,9 +26,13 @@ for base, dirs, files in os.walk('test'):
         if not 'mask' in name:
             print numImg
             img = cv2.imread(os.path.join(base, name))
+
             img = ReducirRuido.denoiseMorfologico(img)
             img = ReducirRuido.denoiseNonLocalMeans(img)
-            img = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=5)
+            img = img_as_ubyte(img)
+
+            #cv2.imshow("imagen",img)
+            #cv2.waitKey(0)
             pa = cascade.detectMultiScale(
                 img,
                 scaleFactor=1.01,
@@ -39,8 +45,7 @@ for base, dirs, files in os.walk('test'):
                 com = (x - 1) * ancho + y
                 fin = ((x - 1) + w) * ancho + y
                 for i in range(com, fin, alto):
-                    hasta = i + h
-                    archivo.write('%d %d ' % (i,hasta))
+                    archivo.write('%d %d ' % (i,h))
                 break
             archivo.write("\n")
             numImg = numImg + 1
